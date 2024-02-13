5631864b7fb8bad716a56ac5fb7570485d4b1845
Piyush-Pant2023 <piyushpant64@gmail.com>
2024-02-13 21:06:17 +0530

    basic

--- practice.js
+++ practice.js
@@ -1,7 +1,6 @@
-let x=1;
-function scopelet(){
-    let x=1;
-    console.log(x)
-}
-scopelet()
-// console.log(x)
+let arr1 = [11, 12, 13]; 
+let arr2 = [14, 15, 16]; 
+let arr3 = [17, 18, 19]; 
+  
+let newArr = arr1.concat(arr2, arr3); 
+console.log(newArr);
