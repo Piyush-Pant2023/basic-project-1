62ab4957c3a19dbc35a453923323ad0ac8d189e7
Piyush-Pant2023 <piyushpant64@gmail.com>
2024-02-14 18:01:23 +0530

    basics

--- practice.js
+++ practice.js
@@ -1,6 +1,10 @@
-let arr1 = [11, 12, 13]; 
-let arr2 = [14, 15, 16]; 
-let arr3 = [17, 18, 19]; 
-  
-let newArr = arr1.concat(arr2, arr3); 
-console.log(newArr);
+function union(arr1, arr2) {
+    // Concatenate the arrays and then convert them to a Set to remove duplicates
+    const combinedArray = [...arr1, ...arr2];
+    const uniqueArray = [...new Set(combinedArray)];
+    // Sort the array in ascending order
+    uniqueArray.sort((a, b) => a - b);
+    return uniqueArray;
+}
+
+console.log(union([1, 2, 3], [100, 2, 1, 10]));
